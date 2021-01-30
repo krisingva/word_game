c047c6cb3a0f2ffe6ee1d0107a7943a740ff539a
Kris Ingva <krisingva@gmail.com>
2021-01-29 20:25:37 -0500

    Initial working word game

---
+++ Gemfile
@@ -0,0 +1,10 @@
+source "https://rubygems.org"
+
+gem "sinatra"
+gem "sinatra-contrib"
+gem "erubis"
+gem "rack-test"
+gem "minitest"
+gem 'psych', '~> 2.0', '>= 2.0.17'
+
+ruby "2.7.1"
---
+++ word.rb
@@ -0,0 +1,104 @@
+require 'sinatra'
+require 'sinatra/reloader'
+require 'tilt/erubis'
+require 'yaml'
+
+configure do
+  enable :sessions
+  set :session_secret, 'super secret'
+end
+
+def load_words
+  path = File.expand_path("../word.yaml", __FILE__)
+  YAML.load_file(path)
+end
+
+def setup_game
+  session[:word] = load_words.sample.upcase
+  session[:placeholder] = create_placeholders(session[:word])
+  session[:available] = ("A".."Z").to_a
+  session[:picked] = []
+  session[:points] = 10
+end
+
+def create_placeholders(word)
+  hash = {}
+  (1..(word.size)).each do |key|
+    hash[key] = "_"
+  end
+  hash
+end
+
+def find_positions_to_change(letter)
+  positions = []
+  session[:word].chars.each_with_index do |char, idx|
+    if char == letter
+      positions << idx + 1
+    end
+  end
+  positions
+end
+
+def change_placeholders(letter)
+  positions = find_positions_to_change(letter)
+  positions.each do |key|
+    session[:placeholder][key] = " #{letter} "
+  end
+end
+
+def word_contains_letter?(letter)
+  session[:word].chars.any? {|char| char == letter }
+end
+
+def deduct_point
+  session[:points] -= 1
+end
+
+def word_guessed?
+  session[:placeholder].none? { |_, value| value == "_"}
+end
+
+get "/" do
+  setup_game
+  erb :home
+end
+
+get "/new" do
+  setup_game
+  redirect "/word"
+end
+
+get "/word" do
+    @word = session[:word]
+    @letter_hash = session[:placeholder]
+    @alphabet = session[:available]
+    @points = session[:points]
+  erb :word
+end
+
+post "/choose_letter" do
+  letter = params[:letter]
+  p letter
+  change_placeholders(letter)
+  session[:available].delete(letter)
+  session[:picked] << letter
+  deduct_point unless word_contains_letter?(letter)
+  if session[:points] == 0
+    session[:message] = "The game is over, the word was #{session[:word]}."
+    redirect "/end"
+  elsif word_guessed?
+    session[:message] = "You guessed the word #{session[:word]}, great job!"
+    redirect "/end"
+  else
+    redirect "/word"
+  end
+end
+
+get "/end" do
+  erb :end
+end
+
+get "/finish" do
+  erb :finish
+end
+
